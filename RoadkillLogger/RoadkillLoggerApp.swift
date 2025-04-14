85fe75f589f2cb61c2ac308c5dcd8b5adb47854c
Erika Barthelmess <barthelmess@stlawu.edu>
2025-04-13 22:16:44 -0400

    Initial Commit

---
+++ RoadkillLogger/RoadkillLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadkillLoggerApp.swift
+//  RoadkillLogger
+//
+//  Created by Erika Barthelmess on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct RoadkillLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RoadkillLoggerTests/RoadkillLoggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoadkillLoggerTests.swift
+//  RoadkillLoggerTests
+//
+//  Created by Erika Barthelmess on 4/13/25.
+//
+
+import Testing
+@testable import RoadkillLogger
+
+struct RoadkillLoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
